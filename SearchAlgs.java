d0c4c9d8bd8ad7894c32176d353aabf29d93bce5
Jillian <jbollard@iwu.edu>
2026-01-12 14:07:08 -0600

    fix returns to System.out.print lines

--- SearchAlgs.java
+++ SearchAlgs.java
@@ -2,28 +2,28 @@
 
 public class SearchAlgs{
     // main function for inputs and searches (will utilize helper class with full search methods)
-    public static int main(int num, String method){
+    public static void main(int num, String method){
 
         // read the numbers in the given command line into a supporting data structure to be traversed
-
+        
 
 
         // check for linear method input
         if(method == "linear"){
             // call linear search and return result
-            return FullSearchAlgs.linear_search(num);
+            System.out.println(FullSearchAlgs.linear_search(num)); 
         }
 
         // check for binary method input
         else if(method == "binary"){
             // call binary search and return result
-            return FullSearchAlgs.binary_search(num);
+            System.out.println(FullSearchAlgs.binary_search(num));
         }
 
         // check for random method input
         else if (method == "random"){
             // call random search and return result
-            return FullSearchAlgs.random_search(num);
+            System.out.println(FullSearchAlgs.random_search(num));
         }
     } 
 } 
