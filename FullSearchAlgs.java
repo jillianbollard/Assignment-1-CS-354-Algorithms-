77e131cd1cafba65253283bfcf13324f913fd05d
Jillian <jbollard@iwu.edu>
2026-01-11 20:31:50 -0600

    basic function flow outline and comments

--- FullSearchAlgs.java
+++ FullSearchAlgs.java
@@ -4,6 +4,9 @@ public class FullSearchAlgs {
         // initalize the default return val to not found
         int return_val = -1;
 
+        // implement linear search method
+
+
         // return the final value if found with linear search, terminated, or not found
         return return_val;
     }
@@ -14,15 +17,24 @@ public class FullSearchAlgs {
         // initalize the default return val to not found
         int return_val = -1;
 
+        // implement binary search method
+
+
         // return the final value if found with binary search, terminated, or not found
         return return_val;
     }
 
 
-    // random search function
+    // random search function *add catch all if the function terminates after 10M searches returning the value -2*
     public static int random_search(int input_num){
         // initalize the default return val to not found
         int return_val = -1;
+
+        // implement random search method
+
+
+        // make catch call where the function terminates and sets the return_val to -2
+        return_val = -2;
 
         // return the final value if found with random search, terminated, or not found
         return return_val;
--- SearchAlgs.java
+++ SearchAlgs.java
@@ -2,24 +2,28 @@
 
 public class SearchAlgs{
     // main function for inputs and searches (will utilize helper class with full search methods)
-    public static void main(int num, String method){
+    public static int main(int num, String method){
+
+        // read the numbers in the given command line into a supporting data structure to be traversed
+
+
 
         // check for linear method input
         if(method == "linear"){
             // call linear search and return result
-            return linear_search(num);
+            return FullSearchAlgs.linear_search(num);
         }
 
         // check for binary method input
         else if(method == "binary"){
             // call binary search and return result
-            return binary_search(num);
+            return FullSearchAlgs.binary_search(num);
         }
 
         // check for random method input
         else if (method == "random"){
             // call random search and return result
-            return random_search(num);
+            return FullSearchAlgs.random_search(num);
         }
     } 
 } 
