4d7f0efa8b106f686b5e8df634e03277624814d0
Jillian <jbollard@iwu.edu>
2026-01-13 11:59:00 -0600

    1/13/2026 in clss editing

--- SearchAlgs.java
+++ SearchAlgs.java
@@ -2,25 +2,28 @@
 
 public class SearchAlgs{
     // main function for inputs and searches (will utilize helper class with full search methods)
-    public static void main(int num, String method){
+    public static void main(String[] args){
+        // grab and store args values but case to ints for later comparison
+        int num = Integer.parseInt(args[0]); 
+        String method = args[1];
 
         // read the numbers in the given command line into a supporting data structure to be traversed
         int[] file_numbers = StdIn.readAllInts();
 
         // check for linear method input
-        if(method == "linear"){
+        if(method.equals("linear")){
             // call linear search and return result
             System.out.println(FullSearchAlgs.linear_search(file_numbers, num)); 
         }
 
         // check for binary method input
-        else if(method == "binary"){
+        else if(method.equals("binary")){
             // call binary search and return result
             System.out.println(FullSearchAlgs.binary_search(file_numbers, num));
         }
 
         // check for random method input
-        else if (method == "random"){
+        else if (method.equals("random")){
             // call random search and return result
             System.out.println(FullSearchAlgs.random_search(file_numbers, num));
         }
--- declaration.md
+++ declaration.md
@@ -73,6 +73,11 @@ detail each separate instance of assistance.
     - **How it was Used:** Used in the random search function to add the previously checked index values to a list so that if all indexes had been checked without finding the value -1 would be returned instead of waiting to hit 10M iterations and then return -2
     - **Specific Part of Assignment Affected:** Random search -1 return instance set up and ability to terminate function early to save time and money from running 10M times if list is only 10 items long and item does not exist within list
 
+- **Instance 3:**
+    - **URL of Source: https://www.geeksforgeeks.org/java/different-ways-for-string-to-integer-conversions-in-java/**
+    - **How it was Used:** Used to cast an int to the String[] args value in index 0 for the number users are searching for in the Integers list read from StdIn.readAllInts()
+    - **Specific Part of Assignment Affected:** Allows variable String[] args input to be casted and stored as a integer in the system so it runs as comparting ints and can match and check to the read ints in the file_numbers array (saves time to convert this to int instead of all file_numbers values to Strings to be able to compare values)
+
 ---
 
 ## Confirmation
